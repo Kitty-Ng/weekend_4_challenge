3c15ec08f56f4b1cfcbfa85f51e9d02f6fc79116
Kitty Ng <kittyng89@gmail.com>
2017-09-23 22:26:19 -0500

    improved styling and added thumbsUpCounter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,14 +4,14 @@ myApp.controller ('ImageController', function(){
     var vm = this;
 
     vm.imgArr = [
-        {imgURL: '/images/crafting.jpg', imgdesc: 'Our first xmas tree in our first home 2016', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/engagement.jpg', imgdesc: 'Engagement in Oqunquit, ME 2015', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/family.jpg', imgdesc: 'Joining the Walders Family 2017', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/friendship.jpg', imgdesc: 'Mac friends from 2007 - now', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/graduation.jpg', imgdesc: 'Graduating from Mac 2011', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/love.jpg', imgdesc: "Zach's favorite photo of us so far 2017", count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/travel.jpg', imgdesc: 'Rome, Italy 2008', count: 0, imgVisible: true, descVisible: false},
-        {imgURL: '/images/wedding.jpg', imgdesc: 'Backyard wedding 2017', count: 0, imgVisible: true, descVisible: false},
+        {imgURL: '/images/crafting.jpg', imgdesc: 'Our first xmas tree in our first home 2016', heartCount: 0, thumbUpCount: 0, imgVisible: true, descVisible: false},
+        {imgURL: '/images/engagement.jpg', imgdesc: 'Engagement in Oqunquit, ME 2015', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/family.jpg', imgdesc: 'Joining the Walders Family 2017', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/friendship.jpg', imgdesc: 'Mac friends from 2007 - now', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/graduation.jpg', imgdesc: 'Graduating from Mac 2011', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/love.jpg', imgdesc: "Zach's favorite photo of us so far 2017", heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/travel.jpg', imgdesc: 'Rome, Italy 2008', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
+        {imgURL: '/images/wedding.jpg', imgdesc: 'Backyard wedding 2017', heartCount: 0, thumbUpCount: 0,imgVisible: true, descVisible: false},
     ]
 
 
@@ -20,7 +20,11 @@ myApp.controller ('ImageController', function(){
     }
 
     vm.loveCounter = function (vm){
-        vm.count = vm.count + 1
+        vm.heartCount = vm.heartCount + 1
+    }
+
+    vm.thumbsUpCounter = function (vm){
+        vm.thumbUpCount = vm.thumbUpCount + 1
     }
 
 });
